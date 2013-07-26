3184b50eb99274f778722df49bfa094639729ced
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2013-07-26 14:47:23 -0400

    Updating manage with all functions

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-
 #Loads a list of the rows in the file f, splitting on the spaces
 def LoadData(fname):
     f=open(fname,'r')
@@ -7,6 +6,7 @@ def LoadData(fname):
         data.append(line.replace('\n','').split(' '))
     f.close()
     return data
+
 #Loads a list of the rows in the file f, splitting on the double spaces
 def LoadDataSpaces(fname):
     f=open(fname,'r')
@@ -15,6 +15,7 @@ def LoadDataSpaces(fname):
         data.append(line.replace('\n','').split('  '))
     f.close()
     return data
+
 #Loads a list of the rows in the file f, splitting on the tabs
 def LoadDataTab(fname):
     f=open(fname,'r')
@@ -23,6 +24,15 @@ def LoadDataTab(fname):
         data.append(line.replace('\n','').split('\t'))
     f.close()
     return data
+
+def LoadDataHyphen(fname):
+    f=open(fname,'r')
+    data=[]
+    for line in f.readlines():
+        data.append(line.replace('\n','').split('-'))
+    f.close()
+    return data
+
 #Used in conjunction with LoadData to pull out a column of a file as a list
 #of floats
 def IterativeFloatAppend(values,index):
@@ -31,6 +41,7 @@ def IterativeFloatAppend(values,index):
         point=float(values[i][index])
         new_list.append(point)
     return new_list
+
 #Used in conjunction with LoadData to pull out a column of a file as a list
 #of integers
 def IterativeIntAppend(values,index):
@@ -39,6 +50,7 @@ def IterativeIntAppend(values,index):
         point=int(values[i][index])
         new_list.append(point)
     return new_list
+
 #Used in conjunction with LoadData to pull out a column of a file as a list
 #of strings
 def IterativeStrAppend(values,index):
@@ -47,6 +59,7 @@ def IterativeStrAppend(values,index):
         point=str(values[i][index])
         new_list.append(point)
     return new_list
+
 #Find the differences in consecutive values of a list
 def Differences(values):
     new_list=[]
@@ -54,6 +67,7 @@ def Differences(values):
         point=values[i+1]-values[i]
         new_list.append(point)
     return new_list
+
 #Remove repeats from a list of values
 def RemoveRepeats(values):
     new_list=[]
@@ -61,8 +75,50 @@ def RemoveRepeats(values):
         if values[i] not in new_list:
             new_list.append(values[i])
     return new_list
+
 #Write a list to file as a column
 def WriteFile(values,fname):
     with open(fname,"w") as data:
         for i in range(len(values)):
-            data.write('{0}\n'.format(values[i]))
+            data.write("{0}\n".format(values[i]))
+
+def WriteFile2Cols(values1,values2,fname):
+    with open(fname,"w") as data:
+        for i in range(len(values1)):
+            data.write("{0} {1}\n".format(values1[i],values2[i]))
+
+def WriteFileCols(values,fname):
+    with open(fname,"w") as data:
+        for i in range(len(values)):
+            data.write("{0} {1}\n".format(values[i][0],values[i][1]))
+
+def WriteFile3Cols(values,fname):
+    with open(fname,"w") as data:
+        for i in range(len(values)):
+            data.write("{0} {1} {2}\n".format(values[i][0],values[i][1],values[i][2]))
+
+def WriteFile5Cols(values,fname):
+    with open(fname,"w") as data:
+        for i in range(len(values)):
+            data.write("{0} {1} {2} {3} {4}\n".format(values[i][0],values[i][1],values[i][2],values[i][3],values[i][4]))
+
+def WriteFile4Cols(values,fname):
+    with open(fname,"w") as data:
+        for i in range(len(values)):
+            data.write("{0} {1} {2} {3}\n".format(values[i][0],values[i][1],values[i][2],values[i][3]))
+
+def WriteFile6Cols(values,fname):
+    with open(fname,"w") as data:
+        for i in range(len(values)):
+            data.write("{0} {1} {2} {3} {4} {5}\n".format(values[i][0],values[i][1],values[i][2],values[i][3],values[i][4],values[i][5]))
+
+def WriteFile7Cols(values,fname):    
+    with open(fname,"w") as data:
+        for i in range(len(values)):
+            data.write("{0} {1} {2} {3} {4} {5} {6}\n".format(values[i][0],values[i][1],values[i][2],values[i][3],values[i][4],values[i][5],values[i][6]))
+
+def WriteFile8Cols(values,fname):
+    with open(fname,"w") as data:
+        for i in range(len(values)
+            data.write("{0} {1} {2} {3} {4} {5} {6} {7}\n".format(values[i][0],values[i][1],values[i][2],values[i][3],values[i][4],values[i][5],values[i][6],values[i][7]))
+
